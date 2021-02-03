df7dffed2a47b70b9b456c44876bb4e22d1b9c03
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2021-02-03 22:57:32 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+let InGredients = () => (
+  <div>
+    <ul>
+      <li>Cornflour</li>
+      <li>Oil</li>
+      <li>Potato</li>
+    </ul>
+  </div>
+);
+
 export default function App() {
   const [like, setLike] = useState(20);
   const [dislike, setDislike] = useState(65);
 
   return (
     <div className="App">
-      <h1>Hello Pramod</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Samosa Recipe</h1>
+
       <button onClick={() => setLike(like + 1)}>👍 {like}</button>
       <button onClick={() => setDislike(dislike - 1)}>👎 {dislike}</button>
       <button onClick={() => setDislike(dislike - 1)}>
         dislikecount {dislike}
       </button>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStcvbr-MHz7WaDXx41azS3wcOPcq9xzp_hiw&usqp=CAU"
+        alt="samosa"
+      ></img>
+      <br />
+      <button>Ingredients</button>
+      <p>{{ InGredients }}</p>
+      <button>Preparation</button>
     </div>
   );
 }
