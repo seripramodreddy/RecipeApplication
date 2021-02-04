9674ea88c43fb9f799be97df7c8dee6b710b08a0
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2021-02-04 22:19:20 +0530

    Listing ingredients

--- src/App.js
+++ src/App.js
@@ -9,16 +9,46 @@ const InputBox = (props) => (
   />
 );
 
-const Box = (props) => (
-  <div class="container">
-    <img src={props.recipe}></img>
-    <li>{props.recipes}</li>
-    <button>AddIngredients</button>
+const Display = (props) => (
+  <div>
+    <li>{props.proc}</li>
   </div>
 );
+
+export function Box(props) {
+  const [procs, setProcs] = useState("");
+  const [proc, setProc] = useState([""]);
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="flex-box">
+          <div class="col-4 small">
+            <img class="img1" src={props.recipe}></img>
+            <input
+              placeHolder="add ingredients"
+              onChange={(event) => setProcs(event.target.value)}
+            />
+            <button
+              onClick={() => {
+                setProc([...proc, procs]);
+              }}
+            >
+              AddIngredients
+            </button>
+            {proc.map((element) => (
+              <li>{element}</li>
+              //<Display proc={element}/>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default function App() {
   const [recipe, setRecipe] = useState("");
   const [recipes, setRecipes] = useState([""]);
+  const [procs, setProcs] = useState("");
 
   return (
     <div className="text-center bg-dark">
@@ -32,8 +62,9 @@ export default function App() {
         >
           Add Recipe
         </button>
+
         {recipes.map((element) => (
-          <Box recipe={element} />
+          <Box recipe={element} setProcs={setProcs} procs={procs} />
         ))}
       </div>
     </div>
