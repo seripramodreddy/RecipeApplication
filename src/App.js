6fc276ba8740e08047268039c15016651a98a2ab
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2021-02-06 00:24:30 +0530

    toggle implementation

--- src/App.js
+++ src/App.js
@@ -10,36 +10,50 @@ const InputBox = (props) => (
 );
 
 const Display = (props) => (
-  <div>
-    <li>{props.proc}</li>
+  <div class="card">
+    <ul class="list-group list-group-flush"></ul>
+    <li class="list-group-item">{props.proc}</li>
   </div>
 );
 
 export function Box(props) {
   const [procs, setProcs] = useState("");
-  const [proc, setProc] = useState([""]);
+  const [proc, setProc] = useState([]);
+  const [ing, setIng] = useState(true);
   return (
-    <div class="container">
-      <div class="row">
-        <div class="flex-box">
-          <div class="col-4 small">
-            <img class="img1" src={props.recipe}></img>
-            <input
-              placeHolder="add ingredients"
-              onChange={(event) => setProcs(event.target.value)}
-            />
-            <button
-              onClick={() => {
-                setProc([...proc, procs]);
-              }}
-            >
-              AddIngredients
-            </button>
-            {proc.map((element) => (
-              <li>{element}</li>
-              //<Display proc={element}/>
-            ))}
+    <div class="row row-cols-3">
+      <div clas="col-sm-6">
+        <div class="card-group">
+          <div class="card bg-primary mb-3">
+            <img class="img1 card-img-top" src={props.recipe}></img>
+            <div class="card-body">
+              <input
+                class="card-link"
+                placeHolder="add ingredients"
+                onChange={(event) => setProcs(event.target.value)}
+              />
+              <button
+                class="card-link"
+                onClick={() => {
+                  setProc([...proc, procs]);
+                }}
+              >
+                AddIngredients
+              </button>
+              <button
+                onClick={() => {
+                  setIng(!ing);
+                }}
+              >
+                Toggle Ingredients
+              </button>
+            </div>
           </div>
+          {proc.length > 0
+            ? ing
+              ? proc.map((element) => <Display proc={element} />)
+              : ""
+            : ""}
         </div>
       </div>
     </div>
@@ -47,8 +61,11 @@ export function Box(props) {
 }
 export default function App() {
   const [recipe, setRecipe] = useState("");
-  const [recipes, setRecipes] = useState([""]);
+  const [recipes, setRecipes] = useState([
+    "https://static.toiimg.com/thumb/54289752.cms?imgsize=495844&width=800&height=800"
+  ]);
   const [procs, setProcs] = useState("");
+  const [toogle, setToggle] = useState(true);
 
   return (
     <div className="text-center bg-dark">
@@ -62,10 +79,18 @@ export default function App() {
         >
           Add Recipe
         </button>
-
-        {recipes.map((element) => (
-          <Box recipe={element} setProcs={setProcs} procs={procs} />
-        ))}
+        <button
+          onClick={() => {
+            setToggle(!toogle);
+          }}
+        >
+          Toggle Recipe
+        </button>
+        {toogle
+          ? recipes.map((element) => (
+              <Box recipe={element} setProcs={setProcs} procs={procs} />
+            ))
+          : ""}
       </div>
     </div>
   );
