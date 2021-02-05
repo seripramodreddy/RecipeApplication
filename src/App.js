c99ddad6e44166b05606e13371a9bed654c46f41
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2021-02-06 01:57:03 +0530

    Styling

--- src/App.js
+++ src/App.js
@@ -4,93 +4,107 @@ import "./styles.css";
 
 const InputBox = (props) => (
   <input
-    placeHolder="add recipe name"
+    placeHolder="add recipe image link"
     onChange={(event) => props.setRecipe(event.target.value)}
   />
 );
 
 const Display = (props) => (
-  <div class="card">
-    <ul class="list-group list-group-flush"></ul>
-    <li class="list-group-item">{props.proc}</li>
-  </div>
+  <>
+    <ul class="list-group"></ul>
+    <li class="list-group-item list-group-item-primary">{props.proc}</li>
+  </>
 );
 
 export function Box(props) {
   const [procs, setProcs] = useState("");
-  const [proc, setProc] = useState([]);
+  const [proc, setProc] = useState(["dosa batter", "oil"]);
   const [ing, setIng] = useState(true);
   return (
-    <div class="row row-cols-3">
-      <div clas="col-sm-6">
-        <div class="card-group">
-          <div class="card bg-primary mb-3">
-            <img class="img1 card-img-top" src={props.recipe}></img>
-            <div class="card-body">
-              <input
-                class="card-link"
-                placeHolder="add ingredients"
-                onChange={(event) => setProcs(event.target.value)}
-              />
-              <button
-                class="card-link"
-                onClick={() => {
-                  setProc([...proc, procs]);
-                }}
-              >
-                AddIngredients
-              </button>
-              <button
-                onClick={() => {
-                  setIng(!ing);
-                }}
-              >
-                Toggle Ingredients
-              </button>
-            </div>
-          </div>
-          {proc.length > 0
-            ? ing
-              ? proc.map((element) => <Display proc={element} />)
-              : ""
-            : ""}
-        </div>
+    <div class="container">
+      <div>
+        <img class="img1" src={props.recipe} alt="food recipe"></img>
+        <br />
+        <br />
+        <input
+          placeHolder="add ingredients"
+          onChange={(event) => setProcs(event.target.value)}
+        />{" "}
+        <br />
+        <br />
+        <button
+          type="button"
+          class="btn btn-success"
+          onClick={() => {
+            setProc([...proc, procs]);
+          }}
+        >
+          AddIngredients
+        </button>
+        <br />
+        <br />
+        <button
+          type="button"
+          class="btn btn-dark"
+          onClick={() => {
+            setIng(!ing);
+          }}
+        >
+          Toggle Ingredients
+        </button>
       </div>
+      {proc.length > 0
+        ? ing
+          ? proc.map((element) => <Display proc={element} />)
+          : ""
+        : ""}
     </div>
   );
 }
 export default function App() {
   const [recipe, setRecipe] = useState("");
   const [recipes, setRecipes] = useState([
-    "https://static.toiimg.com/thumb/54289752.cms?imgsize=495844&width=800&height=800"
+    "https://static.toiimg.com/thumb/54289752.cms?imgsize=495844&width=800&height=800",
+    "https://www.hungryforever.com/wp-content/uploads/2015/04/Featured-image-masala-dosa-recipe.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV__-3abmbiyCo3JjXlzLESUd_jmvbgQ62Rw&usqp=CAU"
   ]);
   const [procs, setProcs] = useState("");
   const [toogle, setToggle] = useState(true);
 
   return (
-    <div className="text-center bg-dark">
-      <div className="container">
-        <InputBox recipe={recipe} setRecipe={setRecipe} />
+    <div>
+      <h1 class="start"> Delicious Dosa Recipes</h1>
 
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <InputBox recipe={recipe} setRecipe={setRecipe} />
         <button
+          type="button"
+          class="btn btn-success"
           onClick={() => {
             setRecipes([...recipes, recipe]);
           }}
         >
-          Add Recipe
+          Add Dosa Recipe
         </button>
+
         <button
+          type="button"
+          class="btn btn-dark"
           onClick={() => {
             setToggle(!toogle);
           }}
         >
           Toggle Recipe
         </button>
-        {toogle
-          ? recipes.map((element) => (
-              <Box recipe={element} setProcs={setProcs} procs={procs} />
-            ))
-          : ""}
+      </div>
+      <div class="container">
+        {toogle ? (
+          recipes.map((element) => (
+            <Box recipe={element} setProcs={setProcs} procs={procs} />
+          ))
+        ) : (
+          <h1 class="end">No Dosa Recipes found</h1>
+        )}
       </div>
     </div>
   );
